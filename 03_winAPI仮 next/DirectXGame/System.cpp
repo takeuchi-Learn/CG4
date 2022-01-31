b63dbc8a8d4836d03f00e839bc568b1a32d6d760
TakeuchiYutaBb <k020g1187@g.neec.ac.jp>
2022-01-31 11:21:33 +0900

    mainに書いていたものをlooperの中にすべて移動(todo ゲームシーンに移動)

--- 03_winAPI仮 next/DirectXGame/System.cpp
+++ 03_winAPI仮 next/DirectXGame/System.cpp
@@ -6,13 +6,38 @@
 #include "Input.h"
 #include "Looper.h"
 
+
+
+#include <memory>
+
+#include "Sound.h"
+
+#include "Sprite.h"
+
+#include "DebugText.h"
+
+#include "Object3d.h"
+
+#include "Collision.h"
+
+#include "Time.h"
+
+#include <sstream>
+#include <iomanip>
+
+#include <DirectXMath.h>
+using namespace DirectX;
+
 namespace {
 	constexpr wchar_t* window_title = L"DirectXƒNƒ‰ƒX‰»";
+
 }
 
 System::System() {
 	WinAPI::create(window_title);
 	DirectXCommon::create(WinAPI::getInstance());
+
+
 }
 
 void System::update() {
