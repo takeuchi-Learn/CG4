4bf64d41d428237342a01c9b1303982c787fa22a
TakeuchiYutaBb <k020g1187@g.neec.ac.jp>
2022-02-01 12:59:03 +0900

    PlayシーンからAL3のCollisionのものを削除、firstフラグが不要になるように変更

--- 03_winAPI仮 next/DirectXGame/PlayScene.cpp
+++ 03_winAPI仮 next/DirectXGame/PlayScene.cpp
@@ -28,20 +28,25 @@
 using namespace DirectX;
 
 namespace {
-	bool firstFlag = true;
 
-#pragma region mainからそのまま移植
-	std::unique_ptr<Sound::SoundCommon> soundCommon;
+#pragma region 音
 
-	std::unique_ptr<Sound> soundData1;
+	Sound::SoundCommon* soundCommon = nullptr;
 
-	// --------------------
-	// ビュー変換行列
-	// --------------------
+	Sound* soundData1 = nullptr;
+
+#pragma endregion 音
+
+#pragma region ビュー変換行列
+
 	XMMATRIX matView;
 	XMFLOAT3 eye(0, 0, -100);   // 視点座標
 	XMFLOAT3 target(0, 0, 0);   // 注視点座標
 	XMFLOAT3 up(0, 1, 0);       // 上方向ベクトル
+
+#pragma endregion ビュー変換行列
+
+#pragma region スプライト
 
 	// --------------------
 	// スプライト共通
@@ -63,55 +68,42 @@ namespace {
 	// デバッグテキスト用のテクスチャ番号を指定
 	constexpr UINT debugTextTexNumber = Sprite::spriteSRVCount - 1;
 
+#pragma endregion スプライト
+
+#pragma region 3Dオブジェクト
 	// 3Dオブジェクト用パイプライン生成
 	Object3d::PipelineSet object3dPipelineSet;
 
-	// --------------------
-	// 当たり判定
-	// --------------------
-	// 球
-	Sphere sphere;
-	//// 平面
-	Plane plane;
-	// 三角形
-	Triangle triangle;
-	// レイ
-	Ray ray;
-
-	// --------------------
-	// ループ前宣言定義
-	// --------------------
-	int animFrameCount = 0; // アニメーションの経過時間カウンター
 	constexpr UINT obj3dTexNum = 0U;
 	std::unique_ptr<Model> model;
 	std::unique_ptr<Object3d> obj3d;
 	constexpr float obj3dScale = 10.f;
 
+#pragma endregion 3Dオブジェクト
 
-	// --------------------
-	// 時間
-	// --------------------
+#pragma region 時間
+
 	std::unique_ptr<Time> timer(new Time());
 
-
-#pragma endregion
+#pragma endregion 時間
 }
 
 void PlayScene::init() {
 	WinAPI::getInstance()->setWindowText("Press SPACE to change scene - now : Play");
 
-#pragma region mainからそのまま移植
-	if (firstFlag) {
-		soundCommon.reset(new Sound::SoundCommon());
-		soundData1.reset(
-			new Sound("Resources/BGM.wav", soundCommon.get())
-		);
+#pragma region 音
 
-		// ビュー変換行列
-		matView = XMMatrixLookAtLH(
-			XMLoadFloat3(&eye), XMLoadFloat3(&target), XMLoadFloat3(&up)
-		);
-	}
+	soundCommon = new Sound::SoundCommon();
+	soundData1 = new Sound("Resources/BGM.wav", soundCommon);
+
+	// ビュー変換行列
+	matView = XMMatrixLookAtLH(
+		XMLoadFloat3(&eye), XMLoadFloat3(&target), XMLoadFloat3(&up)
+	);
+
+#pragma endregion 音
+
+#pragma region スプライト
 
 	// --------------------
 	// スプライト共通
@@ -151,28 +143,11 @@ void PlayScene::init() {
 	// 3Dオブジェクト用パイプライン生成
 	object3dPipelineSet = Object3d::Object3dCreateGraphicsPipeline(DirectXCommon::getInstance()->getDev());
 
-	// --------------------
-	// 球2平面初期化
-	// --------------------
-	// 球の初期値を設定
-	sphere.center = XMVectorSet(0, 2, 0, 1);	// 中心座標
-	sphere.radius = 2.f;	// 半径c
-	// 平面の初期値を設定
-	plane.normal = XMVectorSet(0, 1, 0, 0);	//法線ベクトル
-	plane.distance = 0.f;	// 原点からの距離
-	// 三角形の初期値を設定
-	triangle.p0 = XMVectorSet(-1.f, 0, -1.f, 1);	//左手前
-	triangle.p1 = XMVectorSet(-1.f, 0, +1.f, 1);	//左奥
-	triangle.p2 = XMVectorSet(+1.f, 0, -1.f, 1);	//右手前
-	triangle.normal = XMVectorSet(0, 1, -0, 0);		//法線
-	// レイの初期値を設定
-	ray.start = XMVectorSet(0, 1, 0, 1);	//原点やや上
-	ray.dir = XMVectorSet(0, -1, 0, 0);		//下向き
 
+#pragma endregion スプライト
 
-	// --------------------
-	// ループ前宣言定義
-	// --------------------
+#pragma region 3Dオブジェクト
+
 	model.reset(new Model(DirectXCommon::getInstance()->getDev(),
 						  L"Resources/model/model.obj", L"Resources/model/tex.png",
 						  WinAPI::window_width, WinAPI::window_height, Object3d::constantBufferNum, obj3dTexNum)
@@ -181,20 +156,9 @@ void PlayScene::init() {
 	obj3d->scale = { obj3dScale, obj3dScale, obj3dScale };
 	obj3d->position = { 0, 0, obj3dScale };
 
+#pragma endregion 3Dオブジェクト
 
-	// --------------------
-	// 音声再生
-	// --------------------
-	if (Sound::checkPlaySound(soundData1.get()) == false) {
-		Sound::SoundPlayWave(soundCommon.get(), soundData1.get(), XAUDIO2_LOOP_INFINITE);
-		OutputDebugStringA("PLAAAAAAAAAAAAAAAAY!\n");
-	} else {
-		Sound::SoundStopWave(soundData1.get());
-		OutputDebugStringA("STOOOOOOOOOOOOOOP!\n");
-	}
-#pragma endregion
-
-	if (firstFlag) firstFlag = false;
+	timer->reset();
 }
 
 void PlayScene::update() {
@@ -210,18 +174,18 @@ void PlayScene::update() {
 	if (Input::getInstance()->triggerKey(DIK_0)) {
 		//Sound::SoundStopWave(soundData1);
 
-		if (Sound::checkPlaySound(soundData1.get())) {
-			Sound::SoundStopWave(soundData1.get());
+		if (Sound::checkPlaySound(soundData1)) {
+			Sound::SoundStopWave(soundData1);
 			OutputDebugStringA("STOP\n");
 		} else {
-			Sound::SoundPlayWave(soundCommon.get(), soundData1.get(), XAUDIO2_LOOP_INFINITE);
+			Sound::SoundPlayWave(soundCommon, soundData1, XAUDIO2_LOOP_INFINITE);
 			OutputDebugStringA("PLAY\n");
 		}
 	}
 
 	{
 		std::string stateStr = "STOP []";
-		if (Sound::checkPlaySound(soundData1.get())) {
+		if (Sound::checkPlaySound(soundData1)) {
 			stateStr = "PLAY |>";
 		}
 		debugText.Print(spriteCommon, "SOUND_PLAY_STATE : " + stateStr, 0, debugText.fontHeight * 2);
@@ -266,67 +230,19 @@ void PlayScene::update() {
 	// マウスここまで
 	// --------------------
 
-	// --------------------
-	// 球2平面更新
-	// --------------------
-
-	// レイ移動
 	{
-		XMVECTOR moveZ = XMVectorSet(0, 0, 0.01f, 0);
-		if (Input::getInstance()->hitKey(DIK_DOWN)) ray.start += moveZ;
-		else if (Input::getInstance()->hitKey(DIK_UP)) ray.start -= moveZ;
-
-		XMVECTOR moveX = XMVectorSet(0.01f, 0, 0, 0);
-		if (Input::getInstance()->hitKey(DIK_RIGHT)) ray.start += moveX;
-		else if (Input::getInstance()->hitKey(DIK_LEFT)) ray.start -= moveX;
-	}
-	//stringstreamで変数の値を読み込み整形
-	std::ostringstream raystr;
-	raystr << "lay.start("
-		<< std::fixed << std::setprecision(2)	// 小数点以下2桁まで
-		<< ray.start.m128_f32[0] << ","		// x
-		<< ray.start.m128_f32[1] << ","		// y
-		<< ray.start.m128_f32[2] << ")";	// z
-
-	debugText.Print(spriteCommon, raystr.str(), debugText.fontWidth * 2, debugText.fontHeight * 5, 1.f);
-
-	raystr.str("");
-	raystr.clear();
-	raystr << "FPS : " << DirectXCommon::getInstance()->getFPS();
-	debugText.Print(spriteCommon, raystr.str(), 0, 0);
-
-	// レイと球の当たり判定
-	XMVECTOR inter;
-	float distance;
-	bool hit = Collision::CheckRay2Sphere(ray, sphere, &distance, &inter);
-	if (hit) {
-		debugText.Print(spriteCommon, "HIT", debugText.fontWidth * 2, debugText.fontHeight * 7, 1.f);
-		// stringstreamをリセット、交点座標を埋め込む
-		raystr.str("");
-		raystr.clear();
-		raystr << "inter:("
-			<< std::fixed << std::setprecision(2)
-			<< inter.m128_f32[0] << ","
-			<< inter.m128_f32[1] << ","
-			<< inter.m128_f32[2] << ")";
-
-		debugText.Print(spriteCommon, raystr.str(), debugText.fontWidth * 2, debugText.fontHeight * 8, 1.f);
-
-		raystr.str("");
-		raystr.clear();
-		raystr << "distance:("
-			<< std::fixed << std::setprecision(2)
-			<< distance << ")";
-
-		debugText.Print(spriteCommon, raystr.str(), debugText.fontWidth * 2, debugText.fontHeight * 9, 1.f);
+		std::ostringstream raystr{};
+		raystr << "FPS : " << DirectXCommon::getInstance()->getFPS();
+		debugText.Print(spriteCommon, raystr.str(), 0, 0);
 	}
 
 	{
 		if (Input::getInstance()->hitKey(DIK_R)) timer->reset();
 
 		std::ostringstream tmpStr{};
-		tmpStr << std::fixed << std::setprecision(6) <<
-			(long double)timer->getNowTime() / Time::oneSec << "[s]";
+		tmpStr << "Time : "
+			<< std::fixed << std::setprecision(6)
+			<< (long double)timer->getNowTime() / Time::oneSec << "[s]";
 		debugText.Print(spriteCommon, tmpStr.str(), 0, debugText.fontHeight * 11);
 	}
 
@@ -381,5 +297,7 @@ void PlayScene::draw() {
 }
 
 void PlayScene::fin() {
-	Sound::SoundStopWave(soundData1.get());
+	Sound::SoundStopWave(soundData1);
+	delete soundData1;
+	delete soundCommon;
 }
