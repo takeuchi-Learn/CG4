22133ebfd3b67cdc7614c900fe0412add9ced388
takeuchi-Learn <74523420+takeuchi-Learn@users.noreply.github.com>
2022-07-12 14:11:27 +0900

    ガウスぼかしを追加

--- Solution/Resources/Shaders/PostEffectPS.hlsl
+++ Solution/Resources/Shaders/PostEffectPS.hlsl
@@ -1,70 +1,102 @@
 #include "PostEffect.hlsli"
 
-Texture2D<float4> tex0 : register(t0);   // 0番スロットに設定されたテクスチャ
-Texture2D<float4> tex1 : register(t1);   // 1番スロットに設定されたテクスチャ
-SamplerState smp : register(s0);         // 0番スロットに設定されたサンプラー
+Texture2D<float4> tex0 : register(t0); // 0番スロットに設定されたテクスチャ
+Texture2D<float4> tex1 : register(t1); // 1番スロットに設定されたテクスチャ
+SamplerState smp : register(s0); // 0番スロットに設定されたサンプラー
 
-float fracNoise(float2 coord) {
-	return frac(sin(dot(coord, float2(13.f, 80.f)) + 0.f) * 44000.f);
+float fracNoise(float2 coord)
+{
+    return frac(sin(dot(coord, float2(13.f, 80.f)) + 0.f) * 44000.f);
 }
 
-float vignatte(float2 uv) {
+float vignatte(float2 uv)
+{
 	// UV値の中心からの距離(大きさ)
-	float len = distance(uv, 0.5f);
+    float len = distance(uv, 0.5f);
 
-	return -len * vignIntensity;
+    return -len * vignIntensity;
 }
 
-float noise(float2 uv, float time) {
-	float noiseNum = fracNoise(uv * time * 10.f) - 0.5f;
-	return noiseNum * noizeIntensity;
+float noise(float2 uv, float time)
+{
+    float noiseNum = fracNoise(uv * time * 10.f) - 0.5f;
+    return noiseNum * noizeIntensity;
+}
+
+float Gaussian(float2 drawUV, float2 pickUV, float sigma)
+{
+    float d = distance(drawUV, pickUV);
+    return exp(-(d * d) / (2 * sigma * sigma));
+}
+
+float4 GaussianBlur(Texture2D<float4> tex, float2 uv, float sigma = 0.005, float stepWidth = 0.001)
+{
+    float _ShiftWidth = 0.005f;
+    float _ShiftNum = 3;
+    float4 col = float4(0, 0, 0, 1);
+    float totalWeight = 0;
+    for (float py = -sigma * 2; py <= sigma * 2; py += stepWidth)
+    {
+        for (float px = -sigma * 2; px <= sigma * 2; px += stepWidth)
+        {
+            float2 pickUV = uv + float2(px, py);
+            float weight = Gaussian(uv, pickUV, sigma);
+            col += tex.Sample(smp, pickUV) * weight;
+            totalWeight += weight;
+        }
+    }
+    col.rgb /= totalWeight;
+    return col;
 }
 
 float4 main(VSOutput input) : SV_TARGET
 {
 	// 画面を左右それぞれmosaicNum分割した大きさのモザイクになる
-	float2 uv = floor(input.uv * mosaicNum) / mosaicNum;
+    float2 uv = floor(input.uv * mosaicNum) / mosaicNum;
 	// 時間[s]
-	float time = nowTime / oneSec;
+    float time = nowTime / oneSec;
 
-	float vignNum = vignatte(uv);
+    float vignNum = vignatte(uv);
 
 	// 走査線のようなもの
-	float sSpeed = 8.f;
-	float sDivLevel = 96.f;
-	float sPower = 24.f;
-	float sinNum = uv.y * sDivLevel + time * sSpeed;
-	float sLineNum = fracNoise(float2(time, uv.y)) * sin(sinNum) * sin(sinNum + 0.75f) + 1;
-	sLineNum /= -sPower;
+    float sSpeed = 8.f;
+    float sDivLevel = 96.f;
+    float sPower = 24.f;
+    float sinNum = uv.y * sDivLevel + time * sSpeed;
+    float sLineNum = fracNoise(float2(time, uv.y)) * sin(sinNum) * sin(sinNum + 0.75f) + 1;
+    sLineNum /= -sPower;
 
 	// 走査線
-	float slSpeed = 0.25f;
-	float slSize = 0.03125f;
-	float slPower = 0.0625f;
-	float sbTime = frac(time * slSpeed);
-	float seTime = sbTime + slSize;
+    float slSpeed = 0.25f;
+    float slSize = 0.03125f;
+    float slPower = 0.0625f;
+    float sbTime = frac(time * slSpeed);
+    float seTime = sbTime + slSize;
 
 	// 6.28318f = 2PI
-	float2 slUv = float2(
+    float2 slUv = float2(
 		uv.x + sin(smoothstep(sbTime, seTime, uv.y) *
 				   6.28318f) * slPower,
 		uv.y
 	);
-	uv = slUv;
+    uv = slUv;
 
 	// rgbずらし
-	float rgbUvNum = 0.005f * sin(time * 3.141592653589793f);
-	float4 texColor0 = tex0.Sample(smp, uv);
-	texColor0.r = tex0.Sample(smp, uv + float2(rgbUvNum, 0.f)).r;
-	float4 texColor1 = tex1.Sample(smp, uv);
-	texColor1.r = tex1.Sample(smp, uv + float2(rgbUvNum, 0.f)).r;
+    float rgbUvNum = 0.005f * sin(time * 3.141592653589793f);
+    float4 texColor0 = GaussianBlur(tex0, uv);
+	//texColor0.r = tex0.Sample(smp, uv + float2(rgbUvNum, 0.f)).r;
+    texColor0.r = GaussianBlur(tex0, uv + float2(rgbUvNum, 0.f)).r;
+    float4 texColor1 = GaussianBlur(tex1, uv);
+	//texColor1.r = tex1.Sample(smp, uv + float2(rgbUvNum, 0.f)).r;
+    texColor1.r = GaussianBlur(tex1, uv + float2(rgbUvNum, 0.f)).r;
+
 
 	// tex0とtex1の横縞になるように描画
-	float4 col = lerp(texColor0, texColor1, step(0.05f, fmod(input.uv.y, 0.1f)));
+    float4 col = lerp(texColor0, texColor1, step(0.05f, fmod(input.uv.y, 0.1f)));
 
-	float noiseNum = noise(input.uv, time);
+    float noiseNum = noise(input.uv, time);
 
-	float4 drawCol = float4(col.rgb + sLineNum + vignNum + noiseNum, alpha);
+    float4 drawCol = float4(col.rgb + sLineNum + vignNum + noiseNum, alpha);
 
-	return drawCol;
+    return drawCol;
 }
