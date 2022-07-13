987cc13fd524896c16fad9c263d08aa6a5597522
takeuchi-Learn <74523420+takeuchi-Learn@users.noreply.github.com>
2022-07-13 10:06:55 +0900

    ガウスぼかしを2つめのシェーダーに移動し、1つ目のシェーダーでノイズがかかるようにした

--- Solution/Resources/Shaders/PostEffectPS.hlsl
+++ Solution/Resources/Shaders/PostEffectPS.hlsl
@@ -23,32 +23,6 @@ float noise(float2 uv, float time)
     return noiseNum * noizeIntensity;
 }
 
-float Gaussian(float2 drawUV, float2 pickUV, float sigma)
-{
-    float d = distance(drawUV, pickUV);
-    return exp(-(d * d) / (2 * sigma * sigma));
-}
-
-float4 GaussianBlur(Texture2D<float4> tex, float2 uv, float sigma = 0.005, float stepWidth = 0.001)
-{
-    float _ShiftWidth = 0.005f;
-    float _ShiftNum = 3;
-    float4 col = float4(0, 0, 0, 1);
-    float totalWeight = 0;
-    for (float py = -sigma * 2; py <= sigma * 2; py += stepWidth)
-    {
-        for (float px = -sigma * 2; px <= sigma * 2; px += stepWidth)
-        {
-            float2 pickUV = uv + float2(px, py);
-            float weight = Gaussian(uv, pickUV, sigma);
-            col += tex.Sample(smp, pickUV) * weight;
-            totalWeight += weight;
-        }
-    }
-    col.rgb /= totalWeight;
-    return col;
-}
-
 float4 main(VSOutput input) : SV_TARGET
 {
 	// 画面を左右それぞれmosaicNum分割した大きさのモザイクになる
@@ -83,12 +57,10 @@ float4 main(VSOutput input) : SV_TARGET
 
 	// rgbずらし
     float rgbUvNum = 0.005f * sin(time * 3.141592653589793f);
-    float4 texColor0 = GaussianBlur(tex0, uv);
-	//texColor0.r = tex0.Sample(smp, uv + float2(rgbUvNum, 0.f)).r;
-    texColor0.r = GaussianBlur(tex0, uv + float2(rgbUvNum, 0.f)).r;
-    float4 texColor1 = GaussianBlur(tex1, uv);
-	//texColor1.r = tex1.Sample(smp, uv + float2(rgbUvNum, 0.f)).r;
-    texColor1.r = GaussianBlur(tex1, uv + float2(rgbUvNum, 0.f)).r;
+    float4 texColor0 = tex0.Sample(smp, uv);
+    texColor0.r = tex0.Sample(smp, uv + float2(rgbUvNum, 0.f)).r;
+    float4 texColor1 = tex1.Sample(smp, uv);
+    texColor1.r = tex1.Sample(smp, uv + float2(rgbUvNum, 0.f)).r;
 
 
 	// tex0とtex1の横縞になるように描画
--- Solution/Resources/Shaders/PostEffectPS_2.hlsl
+++ Solution/Resources/Shaders/PostEffectPS_2.hlsl
@@ -4,10 +4,39 @@ Texture2D<float4> tex0 : register(t0);   // 0番スロットに設定されたテクスチャ
 Texture2D<float4> tex1 : register(t1);   // 1番スロットに設定されたテクスチャ
 SamplerState smp : register(s0);         // 0番スロットに設定されたサンプラー
 
+
+
+float Gaussian(float2 drawUV, float2 pickUV, float sigma)
+{
+    float d = distance(drawUV, pickUV);
+    return exp(-(d * d) / (2 * sigma * sigma));
+}
+
+float4 GaussianBlur(Texture2D<float4> tex, float2 uv, float sigma = 0.005, float stepWidth = 0.001)
+{
+    float _ShiftWidth = 0.005f;
+    float _ShiftNum = 3;
+    float4 col = float4(0, 0, 0, 1);
+    float totalWeight = 0;
+    for (float py = -sigma * 2; py <= sigma * 2; py += stepWidth)
+    {
+        for (float px = -sigma * 2; px <= sigma * 2; px += stepWidth)
+        {
+            float2 pickUV = uv + float2(px, py);
+            float weight = Gaussian(uv, pickUV, sigma);
+            col += tex.Sample(smp, pickUV) * weight;
+            totalWeight += weight;
+        }
+    }
+    col.rgb /= totalWeight;
+    return col;
+}
+
 float4 main(VSOutput input) : SV_TARGET
 {
-	float4 texColor0 = tex0.Sample(smp, input.uv);
-	float4 texColor1 = tex1.Sample(smp, input.uv);
+    // ガウスぼかし
+    float4 texColor0 = GaussianBlur(tex0, input.uv);
+    float4 texColor1 = GaussianBlur(tex1, input.uv);
 
 	// tex0とtex1の横縞になるように描画
 	float4 col = lerp(texColor0, texColor1, step(0.05f, fmod(input.uv.y, 0.1f)));
